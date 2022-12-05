0db3179d97458197a3b2cca54523e802c1698b7b
yonatan-h <yon019hl@gmail.com>
2022-12-05 22:20:18 +0300

    added cloud image service

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+//load environment variables
+const dotenv = require("dotenv");
+dotenv.config();
+
+const cloudinary = require("cloudinary").v2;
 const express = require("express");
 const mysql2 = require("mysql2");
-const dotenv = require("dotenv");
 
-//load environment variables
-dotenv.config();
+//image cloud storage setup
+cloudinary.config({ secure: true });
 
 //mysql connection
 const pool = mysql2.createPool({
@@ -22,8 +26,10 @@ app.listen(PORT, () => console.log("listening to port: " + PORT));
 // heart of the matter
 async function handleRequests(req, res) {
   const blogs = await getBlogs();
+  const imageUrl = await uploadImageAndGetUrl("light-bulb.png");
 
   let listItems = "";
+
   for (const blog of blogs) {
     listItems += `
     <li>
@@ -39,6 +45,7 @@ async function handleRequests(req, res) {
     </head>
     <body>
       <h1>Blogs</h1>
+      <img src="${imageUrl}" alt="a light bulb">
       <ol> ${listItems}</ol>
     </body>
   </html>`);
@@ -48,4 +55,21 @@ async function getBlogs() {
   const result = await pool.promise().execute(`SELECT * FROM blog`);
   const rows = result[0];
   return rows;
+}
+async function uploadImageAndGetUrl(fileName) {
+  const filePath = __dirname + "/" + fileName;
+
+  try {
+    const options = {
+      use_filename: true,
+      unique_filename: false,
+      overwrite: true,
+    };
+    const result = await cloudinary.uploader.upload(filePath, options);
+    console.log(result);
+    return result.secure_url;
+  } catch (e) {
+    console.log(e);
+    return "6454";
+  }
 }
