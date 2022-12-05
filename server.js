81cf29070b7fb5a610190dd2e4e5989deb4f6ab0
yonatan-h <yon019hl@gmail.com>
2022-12-05 19:53:59 +0300

    added a mysql db

--- server.js
+++ server.js
@@ -1,6 +1,51 @@
 const express = require("express");
-const app = express();
+const mysql2 = require("mysql2");
+const dotenv = require("dotenv");
 
-app.get("/", (req, res) => res.send("<h1>HI</h1>"));
+//load environment variables
+dotenv.config();
+
+//mysql connection
+const pool = mysql2.createPool({
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+});
+
+//set up web server
+const app = express();
+app.get("/", handleRequests);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("listening to port: " + PORT));
+
+// heart of the matter
+async function handleRequests(req, res) {
+  const blogs = await getBlogs();
+
+  let listItems = "";
+  for (const blog of blogs) {
+    listItems += `
+    <li>
+      <h2>${blog.title}</h2>
+      <p>${blog.content}</p>
+    </li>`;
+  }
+
+  res.send(`
+  <html> 
+    <head>
+      <title> blogs </title>
+    </head>
+    <body>
+      <h1>Blogs</h1>
+      <ol> ${listItems}</ol>
+    </body>
+  </html>`);
+}
+
+async function getBlogs() {
+  const result = await pool.promise().execute(`SELECT * FROM blog`);
+  const rows = result[0];
+  return rows;
+}
